e54c27d0a8282968294cc69f97d52fc114830589
Codi <codingmagesty@gmail.com>
2019-01-31 23:32:43 +0300

    fixed lowercase input

--- flight_app/src/Containers/FlightsTable/FlightsTable.js
+++ flight_app/src/Containers/FlightsTable/FlightsTable.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import { Table, TableHead, TableBody, TableRow, TableCell, Paper } from '@material-ui/core';
 
+import DB from '../../DataBase/database.js';
+
 class FlightsTable extends Component {
+  adjustDB = () => {
+    return DB.filter(flight => flight.status === this.props.currentFilter);
+  }
+
+  filterDBbySubStr = () => {
+    const filterdDB = this.adjustDB();
+
+    if(isNaN(this.props.search)) {
+      return filterdDB.filter(flight => flight.from.toLowerCase().includes(this.props.search.toLowerCase()));
+    } else {
+      return filterdDB.filter(flight => flight.number.includes(this.props.search))
+    }
+  }
+
   render() {
-    if (!this.props.search) {
+    const filterdDB = (!this.props.search) ? this.adjustDB() : this.filterDBbySubStr();
       return(
         <div>
           <Paper>
@@ -11,83 +27,28 @@ class FlightsTable extends Component {
               <TableHead>
                 <TableRow>
                   <TableCell>Номер Рейса</TableCell>
-                  <TableCell>Аэропорт Отправления</TableCell>
+                  <TableCell>Город Отправления</TableCell>
                   <TableCell>Отправление</TableCell>
                   <TableCell>Прибытие</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                <TableRow>
-                  <TableCell>1</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>2</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>3</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>4</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>5</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>6</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>7</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>8</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>9</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>10</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
+              {
+                filterdDB.map(flight => (
+                  <TableRow key={flight.id}>
+                    <TableCell>{flight.number}</TableCell>
+                    <TableCell>{flight.from}</TableCell>
+                    <TableCell>{flight.arrived}</TableCell>
+                    <TableCell>{flight.departured}</TableCell>
+                  </TableRow>
+                ))
+              }
               </TableBody>
             </Table>
           </Paper>
         </div>
       );
-    } else {
-      return (
-        <p>{this.props.search}</p>
-      );
     }
-  }
 }
 
 export default FlightsTable;
---
+++ flight_app/src/DataBase/database.js
@@ -0,0 +1,122 @@
+export default [
+  {
+    'id': 0,
+    'number': 'SU 1234',
+    'from': 'Амстердам',
+    'departured': '12:00',
+    'arrived': '17:00',
+    'status': 'arrived'
+  },
+  {
+    'id': 1,
+    'number': 'SU 4564',
+    'from': 'Челябинск',
+    'departured': '10:25',
+    'arrived': '14:00',
+    'status': 'arrived'
+  },
+  {
+    'id': 2,
+    'number': 'SU 777',
+    'from': 'Люксембург',
+    'departured': '00:00',
+    'arrived': '10:20',
+    'status': 'arrived'
+  },
+  {
+    'id': 3,
+    'number': 'SU 8907',
+    'from': 'Брюссель',
+    'departured': '16:15',
+    'arrived': '00:00',
+    'status': 'arrived'
+  },
+  {
+    'id': 4,
+    'number': 'SU 987',
+    'from': 'Лондон',
+    'departured': '9:00',
+    'arrived': '15:00',
+    'status': 'arrived'
+  },
+  {
+    'id': 5,
+    'number': 'SU 7652',
+    'from': 'Москва',
+    'departured': '11:00',
+    'arrived': '13:30',
+    'status': 'departured'
+  },
+  {
+    'id': 6,
+    'number': 'SU 000',
+    'from': 'Москва',
+    'departured': '20:00',
+    'arrived': '23:20',
+    'status': 'departured'
+  },
+  {
+    'id': 7,
+    'number': 'SU 732',
+    'from': 'Москва',
+    'departured': '9:45',
+    'arrived': '17:00',
+    'status': 'departured'
+  },
+  {
+    'id': 8,
+    'number': 'SU 666',
+    'from': 'Москва',
+    'departured': '01:00',
+    'arrived': '23:00',
+    'status': 'departured'
+  },
+  {
+    'id': 9,
+    'number': 'SU 912',
+    'from': 'Москва',
+    'departured': '8:30',
+    'arrived': '12:40',
+    'status': 'departured'
+  },
+  {
+    'id': 10,
+    'number': 'SU 098',
+    'from': 'Вильнюс',
+    'departured': '17:10',
+    'arrived': '21:40',
+    'status': 'delayed'
+  },
+  {
+    'id': 11,
+    'number': 'SU 323',
+    'from': 'Минск',
+    'departured': '1:00',
+    'arrived': '4:05',
+    'status': 'delayed'
+  },
+  {
+    'id': 12,
+    'number': 'SU 777',
+    'from': 'Токио',
+    'departured': '3:35',
+    'arrived': '8:50',
+    'status': 'delayed'
+  },
+  {
+    'id': 13,
+    'number': 'SU 331',
+    'from': 'Вашингтон',
+    'departured': '12:00',
+    'arrived': '23:40',
+    'status': 'delayed'
+  },
+  {
+    'id': 14,
+    'number': 'SU 987',
+    'from': 'Осло',
+    'departured': '14:00',
+    'arrived': '18:30',
+    'status': 'delayed'
+  },
+]
